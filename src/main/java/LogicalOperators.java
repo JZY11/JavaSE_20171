e4464e7c708fed4361e783d8a046babae9bc2c49
Tony.Jaa <1291813139@qq.com>
2017-03-07 15:20:04 +0800

    update

--- src/main/java/LogicalOperators.java
+++ src/main/java/LogicalOperators.java
@@ -8,11 +8,16 @@ public class LogicalOperators {
 //        System.out.println(b1 & b2);
 //        System.out.println(b1 &/ |/ ^ /!b2);//^b1 b2不相同，结果为ture  !取反  单操作数运算  ！运算级最高
         // &与&&运算规则相同 |与||一样
-        int x = 0;
+        int x = 3;
         int y = 1;
-        boolean z = (x > y)&&(y < x);
+        boolean z = (x > y)&&(y++  <= x);
         System.out.println(z);
         System.out.println(y);
+        int a = 2;
+        int b = 3;
+        boolean c= (a > b)&&(b++<=a);
+        System.out.println(c);
+        System.out.println(b);//条件与的两种情况
 
     }
 }
