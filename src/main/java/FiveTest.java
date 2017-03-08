108e069de370bed25aae0c8e34ab0d60801a45d4
Tony.Jaa <1291813139@qq.com>
2017-03-08 17:11:03 +0800

    update

---
+++ src/main/java/FiveTest.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Tony.Jaa on 2017/3/8.
+ */
+public class FiveTest {
+    public static void main(String[] args) {
+        int x = 95;
+        if (x >= 90) {
+            System.out.println("A");
+        }
+        else if (x >= 60 && x<= 89){
+            System.out.println("B");
+        }
+        else {
+            System.out.println("C");
+        }
+    }
+}
