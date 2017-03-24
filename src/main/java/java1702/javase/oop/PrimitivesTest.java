ee48a26f6fff4b6d0ebbf995bf8cb5343f1e4add
Tony.Jaa <1291813139@qq.com>
2017-03-24 11:55:51 +0800

    update

--- src/main/java/java1702/javase/oop/PrimitivesTest.java
+++ src/main/java/java1702/javase/oop/PrimitivesTest.java
@@ -5,17 +5,18 @@ package java1702.javase.oop;
  * on 2017/3/24.
  * JavaSE_20171.
  */
-public class PrimitivesTest {
-    public static void method(int i) {
-        System.out.println("b:" + i);
-        i++;
-        System.out.println("c:" + i);
+public class PrimitivesTest {//基本数据类型作为方法参数（值传递）
+    public static void method(int x) {//boolean method(boolean b)
+        //b = false;
+        System.out.println("b:" + x);//("b:"+ b)
+        x++;
+        System.out.println("c:" + x);//("c:" +b)
     }
 
     public static void main(String[] args) {
-        int i = 0;
-        System.out.println("a:" + i);
-        method(i);
+        int i = 0;//boolean i = true;
+        System.out.println("a:" + i);//("a:" + i)
+        method(i);//
         System.out.println("d:" + i);
     }
 }
