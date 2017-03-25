26d09e3f5b2db8d59e405a29081fa820c77b6a5f
Tony.Jaa <1291813139@qq.com>
2017-03-25 10:07:37 +0800

    update

---
+++ src/main/java/java1702/javase/oop/Animal.java
@@ -0,0 +1,19 @@
+package java1702.javase.oop;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/3/25.
+ * JavaSE_20171.
+ */
+public class Animal {
+    private String name;
+    private boolean eatMeat;
+    private int age;
+    public void eat(){
+    }
+    public void sleep(){
+    }
+    public void cry(){
+
+    }
+}
