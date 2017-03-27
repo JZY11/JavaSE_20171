89765a76be4f382bcf30aa1ad1fc1d0fb7cc0f36
Tony.Jaa <1291813139@qq.com>
2017-03-27 10:03:40 +0800

    update

--- src/main/java/java1702/javase/oop/BoxTest.java
+++ src/main/java/java1702/javase/oop/BoxTest.java
@@ -5,6 +5,9 @@ package java1702.javase.oop;
  * on 2017/3/27.
  * JavaSE_20171.
  */
+/*
+*     String类型以及基本数据类型的封装类是特例（还是值传递）
+ */
 public class BoxTest {
     public static void main(String[] args) {
         Integer i = 0;
