850167b9db2e1cd578c8fa7484d8c60abde14d34
Tony.Jaa <1291813139@qq.com>
2017-03-27 10:02:53 +0800

    update

---
+++ src/main/java/java1702/javase/oop/BoxTest.java
@@ -0,0 +1,28 @@
+package java1702.javase.oop;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/3/27.
+ * JavaSE_20171.
+ */
+public class BoxTest {
+    public static void main(String[] args) {
+        Integer i = 0;
+      //  Boolean b = true;
+        Double b = 1.2d;
+        System.out.println(i);
+        System.out.println(b);
+    }
+
+}
+ /*
+        java.lang
+            boolean - Boolean
+            byte - Byte
+            char - Character*
+            short - Short
+            int - Integer*
+            long - Long
+            float - Float
+            double - Double
+         */
