ac17ca22691d7b0b54b5e0d280e351495e761334
Tony.Jaa <1291813139@qq.com>
2017-03-27 15:52:02 +0800

    update

--- src/main/java/java1702/javase/collection/ArrayTest.java
+++ src/main/java/java1702/javase/collection/ArrayTest.java
@@ -8,6 +8,7 @@ package java1702.javase.collection;
 public class ArrayTest {
     public static void main(String[] args) {
         String[] strings = new String[10];
+        strings[0] = "test...";
         System.out.println(strings[0]);//下标 索引 index
 
         boolean[] booleans = new boolean[100];
@@ -18,5 +19,12 @@ public class ArrayTest {
 
         double[] doubles = new double[10000];
         System.out.println(doubles[9999]);
+        System.out.println(doubles.length);//输出数组的长度
+
+        ArrayTest[] arrayTests = {new ArrayTest(), new ArrayTest(), new ArrayTest()};
+        //arrayTests[0] = new ArrayTest();
+        System.out.println(arrayTests[0]);
+        System.out.println(arrayTests[1]);
+        System.out.println(arrayTests[2]);
     }
 }
