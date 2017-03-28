b58e015b2a09eda91ace807893867aacbebcc97e
Tony.Jaa <1291813139@qq.com>
2017-03-28 10:11:32 +0800

    update

--- src/main/java/java1702/javase/collection/_2DArray.java
+++ src/main/java/java1702/javase/collection/_2DArray.java
@@ -15,6 +15,39 @@ public class _2DArray {
             }
             System.out.println();
         }
+        /*    不规则的二维数组
+        1, 2, 3, 4, 5,
+        1
+        1, 2, 3
+        1, 2, 3, 4
+         */
+        //  (1)
+        String[][] strings = new String[4][];
+        strings[0] = new String[5];
+        strings[1] = new String[1];
+        strings[2] = new String[3];
+        strings[3] = new String[4];
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = 0; j < strings[i].length; j++) {
+                System.out.print(strings[i] + "\t");
+            }
+            System.out.println();
+        }
+
+        //   (2)
+        double[][] doubles =
+                {
+                        {1d, 2d, 3d, 4d, 5d},
+                        {1d},
+                        {1d, 2d,3d},
+                        {1d, 2d, 3d,4d}
+                };
+        for (int i = 0; i < doubles.length; i++) {
+            for (int j = 0; j < doubles[i].length; j++) {
+                System.out.print(doubles[i] + "\t");
+            }
+            System.out.println();
+        }
 
     }
 }
