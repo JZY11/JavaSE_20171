bd1a45af7459b1d8191c9eb3e1f677b20bb3434e
Tony.Jaa <1291813139@qq.com>
2017-03-28 19:56:32 +0800

    update

--- src/main/java/java1702/javase/collection/StringAPIDemo.java
+++ src/main/java/java1702/javase/collection/StringAPIDemo.java
@@ -47,4 +47,14 @@ class StringAPIDemo3{
         System.out.println(str1.length());
     }
 }
+           //(5)查找一个指定的字符串是否存在
+class StringAPIDemo4{
+    public static void main(String[] args) {
+        String str1 = "abcdefgcgh";
+        System.out.println(str1.indexOf("c"));
+        System.out.println(str1.indexOf("c", 3));//查到返回位置，从第四个开始查找
+        System.out.println(str1.indexOf("x"));
+
+               }
+}
 
