8b25355af2c32a31a10665ae899fd3c2bc636902
Tony.Jaa <1291813139@qq.com>
2017-03-30 09:24:33 +0800

    update

---
+++ src/main/java/java1702/javase/e/E33.java
@@ -0,0 +1,16 @@
+package java1702.javase.e;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/3/30.
+ * JavaSE_20171.
+ */
+/*
+ 打印出杨辉三角形（要求打印出10行如下图）
+    1
+  1 2 1
+ 1 3 3 1
+.........（略）
+ */
+public class E33 {
+}
--- src/main/java/java1702/javase/e/E7.java
+++ src/main/java/java1702/javase/e/E7.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * on 2017/3/30.
  * JavaSE_20171.
  */
-//            输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
+//     输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
 public class E7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -19,5 +19,20 @@ public class E7 {
         int space = 0;
         int digit = 0;
         int special = 0;
+        for (char aChar : chars) {
+            if ((aChar >= 'A' && aChar <= 'Z') ||(aChar >= 'a' && aChar <= 'z')) {
+                english++;
+            } else if (aChar == ' '){
+                space++;
+            }else if (aChar >= '0' && aChar <= '9'){
+                digit++;
+            }else {
+                special++;
+            }
+        }
+        System.out.println(english);
+        System.out.println(space);
+        System.out.println(digit);
+        System.out.println(special);
     }
 }
