b5b0179d9c50607a8fefa7f7537b229228842724
Tony.Jaa <1291813139@qq.com>
2017-03-30 10:41:40 +0800

    update

--- src/main/java/java1702/javase/e/E33.java
+++ src/main/java/java1702/javase/e/E33.java
@@ -1,16 +1,37 @@
 package java1702.javase.e;
 
+import java.util.Arrays;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/3/30.
  * JavaSE_20171.
  */
 /*
- 打印出杨辉三角形（要求打印出10行如下图）
+ 打印出杨辉三角形（要求打印出10行如下图）（数组）
     1
+   1 1
   1 2 1
  1 3 3 1
 .........（略）
  */
 public class E33 {
+    public static void main(String[] args) {
+        int[] array1 = {1};
+        int[] array2 = {1,1};
+        int[] array3 = {1, 2, 1};
+        int[] array4 = {1, 3, 3, 1};
+//        int[] l5 = {1, 4, 6, 4,1};
+        int[] array5 = new int[5];
+        for (int i = 0; i < array5.length; i++) {
+            if (i == 0 || i == array5.length - 1) {
+                array5[i] = 1;
+
+            } else {
+                array5[i] = array4[i - 1] + array4[i];
+            }
+        }
+        System.out.println(Arrays.toString(array5));
+
+    }
 }
--- src/main/java/java1702/javase/e/E7.java
+++ src/main/java/java1702/javase/e/E7.java
@@ -12,14 +12,14 @@ public class E7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("input string:");
-        String string = scanner.nextLine();//读取一行
+        String string = scanner.nextLine();//读取这一行字符
 
         char[] chars = string.toCharArray();//字符串转换为字符数组
         int english = 0;
         int space = 0;
         int digit = 0;
         int special = 0;
-        for (char aChar : chars) {
+        for (char aChar : chars) { //for each循环做个遍历
             if ((aChar >= 'A' && aChar <= 'Z') ||(aChar >= 'a' && aChar <= 'z')) {
                 english++;
             } else if (aChar == ' '){
