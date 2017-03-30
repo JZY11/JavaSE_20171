0424a18b7edfac3e05b6ce1a354dd7bdc8e57ca8
Tony.Jaa <1291813139@qq.com>
2017-03-30 16:51:34 +0800

    update

--- src/main/java/java1702/javase/oop/Student.java
+++ src/main/java/java1702/javase/oop/Student.java
@@ -5,6 +5,13 @@ package java1702.javase.oop;
  * on 2017/3/30.
  * JavaSE_20171.
  */
+/*
+     3.初始化：
+1.动态初始化：数组定义与为数组分配空间和赋值的操作分开进行；
+2.静态初始化：在定义数字的同时就为数组元素分配空间并赋值；
+3.默认初始化：数组是引用类型，它的元素相当于类的成员变量，因此数组分配空间后，每个元素也被按照成员变量的规则被隐士初始化。
+ */
+// set/get方法与域(private)密切相关
 public class Student {
     private String name;
     private int stuNo;
