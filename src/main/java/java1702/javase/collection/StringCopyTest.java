07448ee9121ed3e713640dad4d3503fb93159353
Tony.Jaa <1291813139@qq.com>
2017-03-31 10:54:17 +0800

    update

--- src/main/java/java1702/javase/collection/StringCopyTest.java
+++ src/main/java/java1702/javase/collection/StringCopyTest.java
@@ -12,6 +12,8 @@ public class StringCopyTest {
         int[] srcArray = {1, 2, 3, 4, 5, 6};
         int[] destArray = new int[10];
         System.out.println(Arrays.toString(destArray));//输出一个容量为10的十个0的字符串
+        System.arraycopy(srcArray,0, destArray, 0, 1);
+        System.out.println(Arrays.toString(destArray));
 
     }
 }
