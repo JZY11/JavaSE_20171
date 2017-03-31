319a9913519d707b3eb741fd157051803bfc22ff
Tony.Jaa <1291813139@qq.com>
2017-03-31 14:51:20 +0800

    update

---
+++ src/main/java/java1702/javase/collection/ListTest.java
@@ -0,0 +1,38 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/3/31.
+ * JavaSE_20171.
+ */
+public class ListTest extends ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> strings  = new ArrayList<>();
+        strings.add("hello");
+        strings.add("whatever");
+        strings.add("hello");
+        for (String s : strings) {
+            System.out.println(s);
+        }
+        System.out.println(strings.size());
+        System.out.println(strings.get(0));
+        System.out.println(strings.contains("whatever"));
+        System.out.println(strings.indexOf("hello"));
+        System.out.println(strings.lastIndexOf("hello"));
+        System.out.println(strings.remove(0));
+        System.out.println(strings.get(0));
+
+        System.out.println(strings.set(1, "test"));
+        System.out.println(strings.isEmpty());
+
+
+
+        ListTest listTest = new ListTest();
+        for (int i = 0; i < 10; i++) {
+            listTest.add("" + i);
+        }
+
+    }
+}
