b55d197b5e779cffaed20202190f317a661199ad
Tony.Jaa <1291813139@qq.com>
2017-03-31 10:04:41 +0800

    update

---
+++ src/main/java/java1702/javase/collection/StringCopyTest.java
@@ -0,0 +1,17 @@
+package java1702.javase.collection;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/3/31.
+ * JavaSE_20171.
+ */
+public class StringCopyTest {
+    public static void main(String[] args) {
+        int[] srcArray = {1, 2, 3, 4, 5, 6};
+        int[] destArray = new int[10];
+        System.out.println(Arrays.toString(destArray));//输出一个容量为10的十个0的字符串
+
+    }
+}
