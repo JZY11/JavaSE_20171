30eec9568cf15e23493cb687739883b0fa7ae07f
Tony.Jaa <1291813139@qq.com>
2017-03-31 09:27:02 +0800

    update

--- src/main/java/java1702/javase/collection/ArrayVector.java
+++ src/main/java/java1702/javase/collection/ArrayVector.java
@@ -29,10 +29,30 @@ public class ArrayVector {
 
 //        integers.set(0,1);//将100改为了1
         System.out.println(integers.get(0));
-        System.out.println(integers.set(0,1));//有返回值的   返回修改之前的值100，但其值已经改为1了
+        System.out.println(integers.set(0,1));// get和remove 有返回值的   返回修改之前的值100，但其值已经改为1了
 
 
         //4. 查询   可用get代替(返回任何一个索引处的值  )
 
+    }
+}
+class ArrayVector1{//  使用数组来模拟一个变量   DynamicArray(动态数组)
+    private static final int DEFAULT_CAPACITY = 10;
+    private String[] strings;
+    private int capacity;
+    private int size;
+    public ArrayVector1(){
+        strings = new String[DEFAULT_CAPACITY];
+        capacity =DEFAULT_CAPACITY;
+    }
+    public void add(String string){
+        if (size == capacity) {
+            String[] newStrings = new String[capacity * 2];
+            capacity *= 2;
+            for (String s : strings) {
+
+            }
+            strings = newStrings;
+        }
     }
 }
