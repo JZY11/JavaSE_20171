c9803ab499f555154ddf5c9623cda46f71ff0c24
Tony.Jaa <1291813139@qq.com>
2017-04-01 10:03:28 +0800

    update

--- src/main/java/java1702/javase/newoop/Human.java
+++ src/main/java/java1702/javase/newoop/Human.java
@@ -19,7 +19,8 @@ public class Human {
         System.out.println(name + "吃了" + amount + foods);
 
     }
-    public void sleep(){
+    public int sleep(boolean isPlayGame){
+        return isPlayGame ? 1 : 8;//  三目运算符
 
     }
     public String study(int studyTime){//call&invoke method 调用方法
