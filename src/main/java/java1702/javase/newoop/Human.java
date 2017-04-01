fd6e9449c8377e444fd488affaa4616fff64650c
Tony.Jaa <1291813139@qq.com>
2017-04-01 09:04:40 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/Human.java
@@ -0,0 +1,29 @@
+package java1702.javase.newoop;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/1.
+ * JavaSE_20171.
+ */
+public class Human {
+    private String name;
+    private int age;
+    private char gender;
+    private double weight;
+    private double height;
+
+
+    //    一些构造方法
+    public void eat(){
+
+    }
+    public void sleep(){
+
+    }
+    public void study(){
+
+    }
+    public void work(){
+
+    }
+}
