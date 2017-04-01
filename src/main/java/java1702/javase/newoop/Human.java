1b0f3d3ea63f80401f19929d060b596524afad1e
Tony.Jaa <1291813139@qq.com>
2017-04-01 10:09:12 +0800

    update

--- src/main/java/java1702/javase/newoop/Human.java
+++ src/main/java/java1702/javase/newoop/Human.java
@@ -15,20 +15,28 @@ public class Human {
 
     //    一些构造方法(constructor)
 
-    public void eat(String foods, int amount){
+    public void eat(String foods, int amount) {
         System.out.println(name + "吃了" + amount + foods);
 
     }
-    public int sleep(boolean isPlayGame){
+
+    public int sleep(boolean isPlayGame) {
+//        if (isPlayGame) {
+//            return 1;
+//        } else {
+//            return 8;
+//        }
         return isPlayGame ? 1 : 8;//  三目运算符
 
     }
-    public String study(int studyTime){//call&invoke method 调用方法
+
+    public String study(int studyTime) {//call&invoke method 调用方法
         System.out.println(name + "学习了" + studyTime + "小时。");
         return "一份English homework";
 
     }
-    public void work(){
+
+    public void work() {
 
     }
 }
