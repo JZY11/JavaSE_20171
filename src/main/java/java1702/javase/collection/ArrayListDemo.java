cfb949487a9c3bf1c1013c3f9c3c5fcfe9d6d124
Tony.Jaa <1291813139@qq.com>
2017-04-05 17:12:53 +0800

    update

--- src/main/java/java1702/javase/collection/ArrayListDemo.java
+++ src/main/java/java1702/javase/collection/ArrayListDemo.java
@@ -12,8 +12,15 @@ public class ArrayListDemo {
     public static void main(String[] args) {
         List<String> allList = new ArrayList<>();//声明并实例化List对象
         Collection<String> allCollection = new ArrayList<>();//声明并实例化Collection对象
-        allList.add("Hello");
-        allList.add(0, "World");
+        //(一)：验证增加数据的操作
+        allList.add("Hello");//  从Collection继承的方法
+        allList.add(0, "World");//  List扩充的方法
+        System.out.println(allList);
+
+        allCollection.add("LXH");//  增加数据
+        allCollection.add("www.mldn.cn");
+        allList.addAll(allCollection);//  从Collection继承的方法，增加一组对象
+        allList.addAll(0, allCollection);//  List自定义的方法，增加一组对象
         System.out.println(allList);
     }
 }
