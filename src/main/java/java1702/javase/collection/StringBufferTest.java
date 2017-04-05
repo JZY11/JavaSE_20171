1acb45fce2dda18863bc50a5a5776beb347eff5a
Tony.Jaa <1291813139@qq.com>
2017-04-05 09:13:06 +0800

    update

---
+++ src/main/java/java1702/javase/collection/StringBufferTest.java
@@ -0,0 +1,20 @@
+package java1702.javase.collection;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/5.
+ * JavaSE_20171.
+ */
+public class StringBufferTest {
+    public static void main(String[] args) {
+        StringBuffer buf = new StringBuffer(); //声明StringBuffer对象
+        buf.append("Hello");
+        buf.insert(0, "Hey");
+        buf.append("World!!!");
+        fun(buf);         //   传递StringBuffer引用
+        System.out.println(buf);
+    }
+    public static void fun(StringBuffer s){//接收StringBuffer引用
+        s.append("MLDN").append("LiXingHua");//修改StringBuffer内容
+    }
+}
