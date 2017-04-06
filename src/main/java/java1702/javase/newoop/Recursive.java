b253a752e765ac829b9254dd517588f3e1208f47
Tony.Jaa <1291813139@qq.com>
2017-04-06 17:17:13 +0800

    update

--- src/main/java/java1702/javase/newoop/Recursive.java
+++ src/main/java/java1702/javase/newoop/Recursive.java
@@ -25,4 +25,15 @@ public class Recursive {
         System.out.println(fn(10));
     }
 }
-   //
+   // f(20) = 1, f(21) = 4, f(n + 2) = 2 * f(n + 1) + f(n),求f(10) = ?
+class Recursive1{
+    public static int fn(int n){
+        if (n == 20) {
+            return 1;
+        } else if (n == 21){
+            return 4;
+        }else {
+            return fn(n + 2) - 2 * fn(n + 1);
+        }
+    }
+   }
