3c744d17d16d32bb1bf71cca92e0e5b9310429f0
Tony.Jaa <1291813139@qq.com>
2017-04-06 17:10:35 +0800

    update

--- src/main/java/java1702/javase/newoop/Recursive.java
+++ src/main/java/java1702/javase/newoop/Recursive.java
@@ -8,7 +8,7 @@ package java1702.javase.newoop;
 /*
 *   已知有一个数列：f(0) = 1, f(1) = 4, f(n + 2) = 2 * f(n + 1) + f(n),求f(10)
 *   可以用递归方法求得（一个方法体内调用它自身，被称为方法递归。方法递归包含了
-*   一种隐式循环，它会重复执行某段代码，但这样重复无须循环控制
+*   一种隐式循环，它会重复执行某段代码，但这样重复无须循环控制）
  */
 public class Recursive {
     public static int fn(int n){
@@ -17,7 +17,7 @@ public class Recursive {
         } else if (n == 1){
             return 4;
         }else {
-            return 2 * fn(n - 1) + fn(n - 2);
+            return 2 * fn(n - 1) + fn(n - 2);//fn方法里调用fn的形式
         }
     }
 
@@ -25,3 +25,4 @@ public class Recursive {
         System.out.println(fn(10));
     }
 }
+   //
