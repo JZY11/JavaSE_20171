4906733da6a0f770c2b3a9fbfcf28ba03a6aad6a
Tony.Jaa <1291813139@qq.com>
2017-04-07 15:12:09 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/StringTest.java
@@ -0,0 +1,15 @@
+package java1702.javase.newoop;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/7.
+ * JavaSE_20171.
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s.charAt(4));
+        System.out.println();
+        System.out.println(s.concat("World"));
+    }
+}
