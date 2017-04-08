ad070971e24536c20fde9e98767f4bfe1b3f5a08
Tony.Jaa <1291813139@qq.com>
2017-04-08 10:02:01 +0800

    update

--- src/main/java/java1702/javase/newoop/VectorTest.java
+++ src/main/java/java1702/javase/newoop/VectorTest.java
@@ -14,5 +14,21 @@ public class VectorTest {
         System.out.println(strings.add("hey"));
         System.out.println(strings.get(1));
         System.out.println(strings.size());
+        System.out.println(strings.capacity());
+        strings.clear();
+        System.out.println(strings);
+    }
+}
+class _Vector{
+    public static void main(String[] args) {//初始化向量的容量及容量的增量
+        Vector<String> strings = new Vector<>(4, 2);
+        strings.add("hello");
+        strings.add("hey");
+        strings.add("hi");
+        strings.add("world");
+        strings.add("wow");
+        System.out.println(strings.get(3));
+        System.out.println(strings.size());
+        System.out.println(strings.capacity());
     }
 }
