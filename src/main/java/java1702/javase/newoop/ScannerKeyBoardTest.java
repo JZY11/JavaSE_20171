2b5fbce13a5e3063c7f5481e6c23b75208652923
Tony.Jaa <1291813139@qq.com>
2017-04-09 20:19:38 +0800

    update

--- src/main/java/java1702/javase/newoop/ScannerKeyBoardTest.java
+++ src/main/java/java1702/javase/newoop/ScannerKeyBoardTest.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class ScannerKeyBoardTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
         while (sc.hasNext()){
             System.out.println("键盘输入的内容是：" + sc.next());
         }
