53e49b580b0037ba38dbd75b23e30af69e55e16f
Tony.Jaa <1291813139@qq.com>
2017-04-09 20:29:45 +0800

    update

--- src/main/java/java1702/javase/newoop/ScannerKeyBoardTest.java
+++ src/main/java/java1702/javase/newoop/ScannerKeyBoardTest.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class ScannerKeyBoardTest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.useDelimiter("\n");
+//        sc.useDelimiter("\n"); //"\n"(新行)转义字符，注释掉后若用户输入的有空格的话就会转行输出
         while (sc.hasNext()){
             System.out.println("键盘输入的内容是：" + sc.next());
         }
