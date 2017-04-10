e666abf7da52e558a780011c798b1fbbb02c9b5b
Tony.Jaa <1291813139@qq.com>
2017-04-10 09:00:56 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/HashtableTest.java
@@ -0,0 +1,17 @@
+package java1702.javase.newoop;
+
+import java.util.Hashtable;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/10.
+ * JavaSE_20171.
+ */
+public class HashtableTest {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(1, "hello");
+        hashtable.put(2, "hi");
+        System.out.println(hashtable.size());
+    }
+}
