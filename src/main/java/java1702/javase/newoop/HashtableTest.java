d379b50d8e204da3e7e985d4deaab864d45b34b2
Tony.Jaa <1291813139@qq.com>
2017-04-10 09:05:54 +0800

    update

--- src/main/java/java1702/javase/newoop/HashtableTest.java
+++ src/main/java/java1702/javase/newoop/HashtableTest.java
@@ -12,7 +12,12 @@ public class HashtableTest {
         Hashtable<Integer, String> hashtable = new Hashtable<>();
         hashtable.put(1, "hello");
         hashtable.put(2, "hi");
+        hashtable.put(3,"world");
         System.out.println(hashtable.size());
         System.out.println(hashtable.get(1));
+        System.out.println(hashtable.remove(2));
+        for (Integer integer : hashtable.keySet()) {
+            System.out.println(integer + "------>" + hashtable.get(integer));
+        }
     }
 }
