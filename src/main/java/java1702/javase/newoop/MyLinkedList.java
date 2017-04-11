9afc43a2c7fe36b876ecdc0a8d4f64cb2a3e3b4c
Tony.Jaa <1291813139@qq.com>
2017-04-11 11:24:32 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/MyLinkedList.java
@@ -0,0 +1,53 @@
+package java1702.javase.newoop;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/11.
+ * JavaSE_20171.
+ */
+public class MyLinkedList {
+    private Node first;
+    private Node last;
+
+    public MyLinkedList() {
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add("a");
+        myLinkedList.add("b");
+        System.out.println(myLinkedList.first());
+        System.out.println(myLinkedList.last());
+    }
+
+    public boolean add(String element) {
+        if (first == null) {
+            Node node = new Node(element, null, null);
+            first = node;
+            last = node;
+        } else {
+        }
+        return true;
+    }
+
+    public String first() {
+        return first.element;
+    }
+
+    public String last() {
+        return last.element;
+    }
+
+    private class Node { // node 节点\ [nəʊd]
+        String element;
+        Node prev; // prev previous 以前的\ ['priːvɪəs]
+        Node next;
+
+        public Node(String element, Node prev, Node next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+}
+
