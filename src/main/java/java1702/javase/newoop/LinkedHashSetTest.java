ac2af35c1d728791037b22e42b0bd5219909195f
Tony.Jaa <1291813139@qq.com>
2017-04-11 16:15:33 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/LinkedHashSetTest.java
@@ -0,0 +1,32 @@
+package java1702.javase.newoop;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/11.
+ * JavaSE_20171.
+ */
+public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        LinkedHashSet<String> strings = new LinkedHashSet<>();
+        strings.add("d");
+        strings.add("c");
+        strings.add("b");
+        strings.add("a");
+        HashSet<String> strings1 = new HashSet<>();
+        strings1.add("hello");
+        strings1.add("hey");
+        strings1.add("hi");
+        System.out.println(strings.size());
+        for (String string : strings) {
+            System.out.println(string);
+        }
+        System.out.println(strings.isEmpty());
+        System.out.println(strings.contains("k"));
+        System.out.println(strings.remove("c"));
+        System.out.println("===========");
+        System.out.println(strings.equals(strings1));
+    }
+}
