f5f7f6ef9ac6aa7b4fc0697eb24a50a25d6f81e1
Tony.Jaa <1291813139@qq.com>
2017-04-12 10:12:44 +0800

    update

--- src/main/java/java1702/javase/newoop/TreeMapTest.java
+++ src/main/java/java1702/javase/newoop/TreeMapTest.java
@@ -10,14 +10,24 @@ import java.util.TreeMap;
  */
 public class TreeMapTest {
     public static void main(String[] args) {
-        TreeMap<Integer, String> map = new TreeMap<>();
-        map.put(1, "a");
-        map.put(12, "ab");
-        map.put(123, "cde");
+//        TreeMap<Integer, String> map = new TreeMap<>();
+//        map.put(1, "a");
+//        map.put(12, "ab");
+//        map.put(123, "cde");
+        TreeMap<String, Integer> map = new TreeMap<>();
+        map.put("a", 1);
+        map.put("abc", 10);
+        map.put("abcd", 100);
+        map.put("b", 1000);
+//        System.out.println(map.size());
+//        System.out.println(map.get(123));
+//        for (Map.Entry<Integer, String> integerStringEntry : map.entrySet()) {
+//            System.out.println(integerStringEntry.getKey() + "--->" + integerStringEntry.getValue());
+//        }
         System.out.println(map.size());
-        System.out.println(map.get(123));
-        for (Map.Entry<Integer, String> integerStringEntry : map.entrySet()) {
-            System.out.println(integerStringEntry.getKey() + "--->" + integerStringEntry.getValue());
+        System.out.println(map.get("abcd"));
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            System.out.println(stringIntegerEntry.getKey() + "--->" + stringIntegerEntry.getValue());
         }
     }
 }
