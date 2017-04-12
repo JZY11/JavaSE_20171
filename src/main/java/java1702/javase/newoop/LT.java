2c3f64a23342c3c22de52f8eb3b200beef06e3de
Tony.Jaa <1291813139@qq.com>
2017-04-12 15:09:20 +0800

    update

--- src/main/java/java1702/javase/newoop/LT.java
+++ src/main/java/java1702/javase/newoop/LT.java
@@ -2,6 +2,8 @@ package java1702.javase.newoop;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by zhenya.1291813139.com
@@ -15,9 +17,18 @@ public class LT {
         list.add("b");
         list.add("c");
         System.out.println(list);
-        Collections.reverse(list);
+//        Collections.reverse(list);
         System.out.println(list);
-        Collections.sort(list);
+//        Collections.sort(list);
         System.out.println(list);
+        System.out.println(Collections.binarySearch(list, "c"));// ??
+
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "a");
+        map.put(2, "b");
+        map.put(3, "c");
+        System.out.println(map);
+        System.out.println(Collections.singletonList(map));// ??
     }
 }
