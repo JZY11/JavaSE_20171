9bbf1365e0714d4f7ccd972127beac8d0e0ecbdf
Tony.Jaa <1291813139@qq.com>
2017-04-12 09:07:33 +0800

    update

---
+++ src/main/java/java1702/javase/newoop/HashMapTest.java
@@ -0,0 +1,28 @@
+package java1702.javase.newoop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/12.
+ * JavaSE_20171.
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "a");
+        map.put(2, "bcd");
+        map.put(3, "def");
+        System.out.println(map.size());
+        for (Integer integer : map.keySet()) {
+            System.out.println(integer + "-->" + map.get(integer));
+        }
+        for (String s : map.values()) {
+            System.out.println(s);
+        }
+        for (Map.Entry<Integer, String> integerStringEntry : map.entrySet()) {
+            System.out.println(integerStringEntry.getKey() +"-->" +integerStringEntry.getValue());
+        }
+    }
+}
