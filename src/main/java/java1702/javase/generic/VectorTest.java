4169d1f0e9ea1eddc26043a2fd97540ea502cfd7
Tony.Jaa <1291813139@qq.com>
2017-04-13 10:09:14 +0800

    update

--- src/main/java/java1702/javase/generic/VectorTest.java
+++ src/main/java/java1702/javase/generic/VectorTest.java
@@ -9,11 +9,19 @@ import java.util.Vector;
  */
 public class VectorTest {
     public static void main(String[] args) {
-        Vector vector = new Vector();
+//        Vector vector = new Vector();
+//        vector.add("abc");
+//        vector.add("123");
+//        vector.add(true);
+//        vector.add(456);
+//        String s = (String) vector.get(3);// 编译不报错运行报错(整型不能强转为字符串)
+//        System.out.println(s);
+//        System.out.println(vector.get(3));
+
+        Vector<String> vector = new Vector<>();
         vector.add("abc");
         vector.add("123");
-        vector.add(true);
-        vector.add(456);
-        System.out.println(vector.get(3));
+        vector.add("true");
+        System.out.println(vector.get(2));
     }
 }
