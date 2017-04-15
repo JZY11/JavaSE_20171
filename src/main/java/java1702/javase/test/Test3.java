876d0c55f35a1e00fe7fa7c2ec8cfbda49905cda
Tony.Jaa <1291813139@qq.com>
2017-04-15 09:32:31 +0800

    update

--- src/main/java/java1702/javase/test/Test3.java
+++ src/main/java/java1702/javase/test/Test3.java
@@ -1,5 +1,8 @@
 package java1702.javase.test;
 
+import java.util.Arrays;
+import java.util.TreeSet;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/4/15.
@@ -8,7 +11,16 @@ package java1702.javase.test;
 public class Test3 {
     public static void main(String[] args) {
         String s0 = "123,45,25,85";
-        s0.split(",");
-        System.out.println(s0);
+        String[] strings = s0.split(",");
+        System.out.println(Arrays.toString(strings));// 可输出字符数组， Arrays工具类
+        int[] a=new int[strings.length];
+
+        for(int i=0;i<a.length;i++)
+            a[i]=Integer.parseInt(strings[i]);
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        for(int number:a)
+            System.out.print(number+" ");
+
     }
 }
