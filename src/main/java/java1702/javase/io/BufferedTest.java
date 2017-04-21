33840854f50744dfebe803e8d3941a0ddf7b863a
Tony.Jaa <1291813139@qq.com>
2017-04-21 20:51:58 +0800

    update

--- src/main/java/java1702/javase/io/BufferedTest.java
+++ src/main/java/java1702/javase/io/BufferedTest.java
@@ -13,8 +13,8 @@ import java.io.*;
 public class BufferedTest {
     public static void main(String[] args) { // buffered 缓冲(提高输入输出效率)
         try(  //若去掉声明的两个资源前的Buffered则copy的一个大型视频&音频的速度会慢的多
-                BufferedInputStream in = new BufferedInputStream(new FileInputStream("C:/Users/Tony.Jaa/Desktop/0420_Download.mov"));
-                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("C:/Users/Tony.Jaa/Desktop/new_Download.mov"))
+                BufferedInputStream in = new BufferedInputStream(new FileInputStream("C:/Users/Tony.Jaa/Desktop/你就不要想起我-田馥甄.mp3"));
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("C:/Users/Tony.Jaa/Desktop/你就不要想起我.mp3"))
                 ) {
             int i;
             while ((i = in.read()) != -1){
---
+++ src/main/java/java1702/javase/io/FileTest.java
@@ -0,0 +1,32 @@
+package java1702.javase.io;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/21.
+ * JavaSE_20171.
+ */
+public class FileTest {
+    // File类  文件(包括文件和目录) 文件和目录路径名的抽象表示形式
+    public static void main(String[] args) {
+        File file = new File("raf");
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());//false (当前不是目录)
+        System.out.println(file.canRead());// 是否可读
+        System.out.println(file.canWrite());// 是否可写
+        try{
+            file.createNewFile();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+//        file.delete();
+//        file.deleteOnExit();
+
+        System.out.println(file.getAbsoluteFile());
+        System.out.println(file.getAbsolutePath());
+
+
+    }
+}
