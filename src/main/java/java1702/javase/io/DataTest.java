322416d942e93ae4f3f28a2a2a478d959cbd22a7
Tony.Jaa <1291813139@qq.com>
2017-04-21 19:52:00 +0800

    update

---
+++ src/main/java/java1702/javase/io/DataTest.java
@@ -0,0 +1,22 @@
+package java1702.javase.io;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/21.
+ * JavaSE_20171.
+ */
+/*
+    若仅限于写数据的话这两个类很少用，因这两个的方法基本在RandonAccessFile类里都有所以可基本取代
+ */
+public class DataTest {
+    public static void main(String[] args) {
+        DataInputStream dataInputStream = new DataInputStream(System.in);
+        DataOutputStream dataOutputStream = new DataOutputStream(System.out);
+//        dataInputStream.
+//        dataOutputStream.
+//        RandomAccessFile
+    }
+}
