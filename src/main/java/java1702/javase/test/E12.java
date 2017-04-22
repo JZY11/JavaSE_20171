78e89fd17d29c8522d7f826617723f815090649b
Tony.Jaa <1291813139@qq.com>
2017-04-22 11:29:52 +0800

    update

--- src/main/java/java1702/javase/test/E12.java
+++ src/main/java/java1702/javase/test/E12.java
@@ -5,6 +5,9 @@ package java1702.javase.test;
  * on 2017/4/22.
  * JavaSE_20171.
  */
+/*
+     编写一个正确处理的产生空指针异常的程序
+ */
 public class E12 {
     public static void main(String[] args) {
         String s = null;
---
+++ src/main/java/java1702/javase/test/E15.java
@@ -0,0 +1,12 @@
+package java1702.javase.test;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/4/22.
+ * JavaSE_20171.
+ */
+public class E15 {
+    public static void main(String[] args) {
+
+    }
+}
