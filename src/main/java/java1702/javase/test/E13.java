94fdd7e221caa506431a5f57dbc46ad2410ec2e4
Tony.Jaa <1291813139@qq.com>
2017-04-22 11:30:35 +0800

    update

--- src/main/java/java1702/javase/test/E13.java
+++ src/main/java/java1702/javase/test/E13.java
@@ -7,6 +7,10 @@ import java.io.*;
  * on 2017/4/22.
  * JavaSE_20171.
  */
+/*
+      编写一个程序,其功能是将两个文件的内容合并到一个文件中
+      例如：1.txt 内容为 abc；2.txt 内容为 def；生成新文件3.txt 内容为 abcdef
+ */
 public class E13 {
     public static void main(String[] args) {
         try (
