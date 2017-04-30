84af5d76a370a69e5fcb28fd4e3542042c0158c3
Tony.Jaa <1291813139@qq.com>
2017-04-30 22:28:37 +0800

    update

--- src/main/java/java1702/javase/tt/t2.java
+++ src/main/java/java1702/javase/tt/t2.java
@@ -1,5 +1,10 @@
 package java1702.javase.tt;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/4/30.
@@ -10,21 +15,45 @@ package java1702.javase.tt;
  */
 public class t2 {
     public static void main(String[] args) {
-        String s = "aabbbcccddddfffff";
-        int max = 0;
-        char c = 0;
+        String s = "aabbbcccddddffff";
+        Hashtable<Character, Integer> hashtable = new Hashtable<>();
         for (int i = 0; i < s.length(); i++) {
-            int counter = 0;
-            for (int j = 0; j < s.length(); j++) {
-                if(s.charAt(i) == s.charAt(j)){
-                    counter++;
-                }
-            }
-            if (counter >= max) {
-                max = counter;
-                c = s.charAt(i);
-            }
+            char c = s.charAt(i);
+            hashtable.put(c, hashtable.get(c)==null?1:hashtable.get(c) + 1);
         }
-        System.out.println(c + ":" + max);
+        int max = Collections.max(hashtable.values());
+//        int max = 0;
+//        for (Map.Entry<Character, Integer> entry : hashtable.entrySet()) {
+//            if(max < entry.getValue()){
+//                max = entry.getValue();
+//            }
+//        }
+        System.out.println(max);
+
+        for (Map.Entry<Character, Integer> entry : hashtable.entrySet()) {
+            if (max == entry.getValue()) {
+                System.out.println(entry.getKey());
+            }
+
+        }
+
+
+
+
+//        int max = 0;
+//        char c = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            int counter = 0;
+//            for (int j = 0; j < s.length(); j++) {
+//                if(s.charAt(i) == s.charAt(j)){
+//                    counter++;
+//                }
+//            }
+//            if (counter >= max) {
+//                max = counter;
+//                c = s.charAt(i);
+//            }
+//        }
+//        System.out.println(c + ":" + max);
     }
 }
