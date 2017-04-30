5b1331b940e754c5fd1b6c088fe0aae416486013
Tony.Jaa <1291813139@qq.com>
2017-05-01 02:32:42 +0800

    update

--- src/main/java/java1702/javase/tt/ConvertTest.java
+++ src/main/java/java1702/javase/tt/ConvertTest.java
@@ -9,8 +9,20 @@ import java.util.Arrays;
  */
 public class ConvertTest {
     public static void main(String[] args) {
+        System.out.println("以下字符串转字符串数组");
         String s = "1,2,3";
         String[] strings = s.split(",");
         System.out.println(Arrays.toString(strings));
+
+
+        System.out.println("以下转int型数组");
+//        int[] ints = Arrays.stream(strings).mapToInt(Integer::parseInt).toArray();
+
+        int[] ints = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            String string = strings[i];
+            ints[i] = Integer.parseInt(string);// 对每个元素赋值加转换
+        }
+            System.out.println(Arrays.toString(ints));
     }
 }
