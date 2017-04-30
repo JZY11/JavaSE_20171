80eb52de840ce1881fbdc01ff3bbbcf305ffa69d
Tony.Jaa <1291813139@qq.com>
2017-04-30 21:50:02 +0800

    update

--- src/main/java/java1702/javase/tt/t1.java
+++ src/main/java/java1702/javase/tt/t1.java
@@ -5,6 +5,12 @@ package java1702.javase.tt;
  * on 2017/4/30.
  * JavaSE_20171.
  */
+/*
+   输入字符串长度len1，字符串s1，字符串长度len2，字符串s2，从后向前比较
+   以短字符串为标准，输出不同元素的个数。
+   例如：s1=“1,3,5” len1=3  s2=“2,4,1,7,5” len2=5
+   输出：1
+ */
 public class t1 {
     public static void main(String[] args) {
         String s1 = "1,3,5,6,7,8";
