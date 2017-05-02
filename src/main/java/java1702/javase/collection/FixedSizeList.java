d2d8db471b0e2436139eae48f103823f49b70f11
Tony.Jaa <1291813139@qq.com>
2017-05-02 15:54:43 +0800

    update

--- src/main/java/java1702/javase/collection/FixedSizeList.java
+++ src/main/java/java1702/javase/collection/FixedSizeList.java
@@ -14,5 +14,10 @@ public class FixedSizeList {
                        //工具类Arrays的asList()方法 可将一个数组或者指定个数的对象转换为一个List集合
         //获取fixedList的实现类，将输出Arrays$ArrayList
         System.out.println(fixedList.getClass());
+//        fixedList.forEach(System.out::println);// 使用方法引用遍历集合元素
+        //试图增加、删除元素都会引发异常
+        fixedList.add("疯狂Android讲义");
+        fixedList.remove("疯狂Java讲义");
+
     }
 }
