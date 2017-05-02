9afd888f156f551b36c0df0ce2e1424b3eafeb00
Tony.Jaa <1291813139@qq.com>
2017-05-02 15:45:16 +0800

    update

---
+++ src/main/java/java1702/javase/collection/FixedSizeList.java
@@ -0,0 +1,18 @@
+package java1702.javase.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/5/2.
+ * JavaSE_20171.
+ */
+public class FixedSizeList {
+    public static void main(String[] args) {
+        List fixedList = Arrays.asList("疯狂Java讲义", "疯狂ios讲义");
+                       //工具类Arrays的asList()方法 可将一个数组或者指定个数的对象转换为一个List集合
+        //获取fixedList的实现类，将输出Arrays$ArrayList
+        System.out.println(fixedList.getClass());
+    }
+}
