66d262a2420696916f08179e379827fcd34e49aa
Tony.Jaa <1291813139@qq.com>
2017-05-02 16:06:41 +0800

    update

--- src/main/java/java1702/javase/collection/FixedSizeList.java
+++ src/main/java/java1702/javase/collection/FixedSizeList.java
@@ -8,10 +8,15 @@ import java.util.List;
  * on 2017/5/2.
  * JavaSE_20171.
  */
+/*
+   工具类Arrays的asList()方法 可将一个数组或者指定个数的对象转换为一个List集合
+   这个List集合既不是ArrayList实现类的实例，也不是Vector实现类的实例，而是Arrays的内部类ArrayList的实例
+   Arrays.ArrayList是一个固定长度的List集合，程序只能遍历访问该集合里的元素
+   不可增加、删除该集合里的元素
+ */
 public class FixedSizeList {
     public static void main(String[] args) {
         List fixedList = Arrays.asList("疯狂Java讲义", "疯狂ios讲义");
-                       //工具类Arrays的asList()方法 可将一个数组或者指定个数的对象转换为一个List集合
         //获取fixedList的实现类，将输出Arrays$ArrayList
         System.out.println(fixedList.getClass());
 //        fixedList.forEach(System.out::println);// 使用方法引用遍历集合元素
