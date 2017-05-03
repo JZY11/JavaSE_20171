424058668a32a69e27b5b1f9117b10e6c95364c1
Tony.Jaa <1291813139@qq.com>
2017-05-03 23:29:37 +0800

    update

--- src/main/java/java1702/javase/tt/LoopTest.java
+++ src/main/java/java1702/javase/tt/LoopTest.java
@@ -1,5 +1,9 @@
 package java1702.javase.tt;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by zhenya.1291813139.com
  * on 2017/5/3.
@@ -12,6 +16,27 @@ public class LoopTest {
             if (i != 1) {
                 System.out.println(ints[i]);
             }
+        }
+        System.out.println("--------");
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+        List<Integer> list = new ArrayList<>();
+        list.add(123);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(list.get(i));
+        }
+        System.out.println("========");
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+        System.out.println("-=-=-=-=-=-=-");
+        Iterator<Integer> iterator = list.iterator();// 迭代器循环
+                                                    //  list对象调用iterator()fangfa
+                                                    // 返回Iterator<Integer>
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            System.out.println(next);
         }
     }
 }
