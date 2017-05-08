3fe0e009352a3dc081f09e11aafb6dafef397cb8
Tony.Jaa <1291813139@qq.com>
2017-05-09 01:15:39 +0800

    update

--- src/main/java/java1702/javase/newIo/OutputStreamTest.java
+++ src/main/java/java1702/javase/newIo/OutputStreamTest.java
@@ -11,7 +11,7 @@ import java.io.OutputStream;
  * JavaSE_20171.
  */
 /*
-         写文件
+         写文件   四个抽象类都实现了closeable接口，所以每当打开一个资源后都要对其进行关闭
     OutputStream仍然不可写中文，同样是基于字节一个一个的写入
     要想写中文可用Writer
  */
---
+++ src/main/java/java1702/javase/newIo/WriterTest.java
@@ -0,0 +1,24 @@
+package java1702.javase.newIo;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/5/9.
+ * JavaSE_20171.
+ */
+public class WriterTest {
+    public static void main(String[] args) {
+        try {
+            Writer writer = new FileWriter("new");
+            writer.write(Integer.parseInt("4e00",16));
+            writer.write(0x4e01);
+            System.out.println(011);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
