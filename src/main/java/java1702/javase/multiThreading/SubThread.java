d402fe809f449672f825d9557056f2f43d9c8f85
Tony.Jaa <1291813139@qq.com>
2017-05-08 10:20:48 +0800

    update

--- src/main/java/java1702/javase/multiThreading/SubThread.java
+++ src/main/java/java1702/javase/multiThreading/SubThread.java
@@ -14,6 +14,8 @@ public class SubThread extends Thread {
 
     @Override
     public void run() {
-        super.run();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
