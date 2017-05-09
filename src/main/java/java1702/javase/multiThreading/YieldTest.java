6281954635590d5b5e339479803e7ba958cee057
Tony.Jaa <1291813139@qq.com>
2017-05-09 09:13:50 +0800

    update

---
+++ src/main/java/java1702/javase/multiThreading/YieldTest.java
@@ -0,0 +1,30 @@
+package java1702.javase.multiThreading;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/5/9.
+ * JavaSE_20171.
+ */
+/*
+        yield让步，让步的为CPU时间片(time slice)
+ */
+public class YieldTest implements Runnable{
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new YieldTest(),"thread1");
+        Thread thread2 = new Thread(new YieldTest(), "thread2");
+        thread1.start();
+        thread2.start();
+
+        System.out.println("test...");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + ":" + Thread.currentThread().getName() + "is running...");
+            if (i % 10 == 0) {
+                Thread.yield();
+            }
+        }
+    }
+}
